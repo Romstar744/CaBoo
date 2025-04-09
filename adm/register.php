6afc9247ca02931e4c2bda60a6ba8db2cc3a1ae5
Roman <romstar744@gmail.com>
2025-04-09 21:13:52 +0300

    Re-design authorization form, personal page

--- adm/register.php
+++ adm/register.php
@@ -1,6 +1,3 @@
-<?php
-session_start();
-?>
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -8,6 +5,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Регистрация</title>
     <link rel="stylesheet" href="../css/register.css">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body>
